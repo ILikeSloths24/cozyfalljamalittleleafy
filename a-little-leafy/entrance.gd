03670cfd1098316e27d1b653bcefdc9df5ba1b0b
JackW~ <jack.withy@outlook.com>
2026-09-26 15:30:53 +1200

    code for entering house

--- a-little-leafy/entrance.gd
+++ a-little-leafy/entrance.gd
@@ -6,7 +6,9 @@ var player_nearby := false
 
 func _process(_delta):
 	if player_nearby and Input.is_action_just_pressed("interact"):
-		print("ENTER HOUSE")
+		get_tree().change_scene_to_file("res://scenes/house interior.tscn")
+
+
 
 func _on_body_entered(body):
 	if body.name == "Player":
